02c4a21cda6e0bc1b4faa71cba6389965aa1203f
Juan Guerrero <juanmatiasg@hotmail.com>
2019-04-06 18:42:20 -0300

    Se Agrego el for

--- src/Persona.java
+++ src/Persona.java
@@ -2,8 +2,11 @@
 public class Persona {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hola mundo ");
+		
+		for (int i=0 ;i<30;i++) {
+			System.out.println("hola mundo ");
+		}
 	}
 
 }
